739f76b3b0353669d6d5b7f8b83516cc1df0652b
michalgmazurek <michalgmazurek@gmail.com>
2018-04-01 13:20:02 +0200

    Done

---
+++ zadanie1.js
@@ -0,0 +1,28 @@
+const findLongestIntegerGap = (number) => {
+  const binary = number.toString(2);
+  console.log('--------------------------------');
+  console.log(binary, ' => Binary representation of : ' + number);
+  console.log('--------------------------------');
+
+  let longestGap = 0;
+  let currentGap = 0;
+
+  for (let i = 0; i < binary.length; i++) {
+    // console.log(i + ' i')
+    // console.log(binary[i] + ' binary[i]')
+    // console.log(longestGap + 'longestGap');
+    // console.log(currentGap + 'currentGap');
+    
+    if (binary[i] === '0') {
+      currentGap++;
+    } else if (binary[i] === '1') {
+      if (currentGap > longestGap) {
+        longestGap = currentGap;
+      }
+      currentGap = 0;
+    }
+  }
+  console.log(longestGap);
+}
+
+findLongestIntegerGap(9132);
